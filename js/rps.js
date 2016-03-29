5476f247e046f4e12eba13c2606e19beecbc7952
letsplayagame <kaangonel@gmail.com>
2016-03-29 16:05:51 +0300

    Update rps.js

--- js/rps.js
+++ js/rps.js
@@ -1,1 +1,18 @@
 
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function computerMove() {
+    return getRandomNumber(1,3);
+}
+
+function checkRound(playerMove, computerMove) {
+    var winner;
+    if(playerMove == computerMove)
+      return "draw";
+    else if(playerMove == "rock")
+      return "player";
+    else if(playerMove == "paper")
+      return "computer";
+}
