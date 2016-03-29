13d9d6b9dbf1f4588db1dea8116edb848436f69a
letsplayagame <kaangonel@gmail.com>
2016-03-29 22:17:11 +0300

    main logic

--- js/rps.js
+++ js/rps.js
@@ -1,18 +1,42 @@
+var theScore = 0;
+var topScore = 0;
 
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function computerMove() {
-    return getRandomNumber(1,3);
+    var possibleValues = ["rock", "paper", "scissors"];
+    var returnValue = possibleValues[getRandomNumber(0, 2)];
+    return returnValue;
 }
 
 function checkRound(playerMove, computerMove) {
-    var winner;
-    if(playerMove == computerMove)
-      return "draw";
-    else if(playerMove == "rock")
-      return "player";
-    else if(playerMove == "paper")
-      return "computer";
+
+    if (playerMove == computerMove)
+        return "draw";
+    else if (playerMove == "rock" && computerMove == "paper")
+        return "computer";
+    else if (playerMove == "paper" && computerMove == "scissors")
+        return "computer";
+    else if (playerMove == "scissors" && computerMove == "rock")
+        return "computer";
+    else
+        return "player"
+}
+
+function updateScore(winner) {
+    var newScore = theScore;
+    //cheep way to initialize the local variable
+    if (winner == "player")
+        newScore = newScore + 1;
+    else if (winner == "computer")
+        newScore = newScore - 1;
+
+    theScore = newScore;
+    
+    if (theScore>topScore)
+        topScore = newScore;    
+    
+    return newScore;
 }
